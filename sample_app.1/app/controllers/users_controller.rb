2b37f2bc8323571683289f5ed0ec2d8ea93e26e0
aaguerra11 <aaguerra11@gmail.com>
2016-08-09 08:39:04 +0000

    asfdf

---
+++ sample_app.1/app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def new
+  end
+  def estudiante
+  end
+  def registrar
+  end
+  def cuestionario
+  end
+end
--- sample_app.1/config/routes.rb
+++ sample_app.1/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
-  get 'users/new'
 
   root 'static_pages#home' # asi se setea la pagina de inicio
 
@@ -8,6 +7,9 @@ Rails.application.routes.draw do
   get  '/about' ,    to:  'static_pages#about' 
   get   '/contact' ,  to:  'static_pages#contact' 
   get  '/signup',  to: 'users#new'
+  get  '/estudiante/login',  to: 'users#estudiante' , as: 'estudiante'
+  get  '/profesor/registrar',  to: 'users#registrar' , as: 'registrar'
+  get  '/estudiante/cuestionario',  to: 'users#cuestionario' , as: 'cuestionario'
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
